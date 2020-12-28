ab32e00e8edb11858e38a5e09ce0423a0620ef7a
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-12-28 22:40:16 +0000

    Adjust 'Create table of contents.vb'

--- 01 - Excel/Solutions/Create Table of Contents.vb
+++ 01 - Excel/Solutions/Create Table of Contents.vb
@@ -1,3 +1,5 @@
+'Add the hyperlinks for each individual worksheet name. To include chart sheets, use the 'Sheets' collection
+
 Sub CreateTableOfContentsPage()
 
 Dim i As Integer
@@ -7,7 +9,6 @@ For i = 2 To Worksheets.Count
   ActiveSheet.Hyperlinks.Add _
 	 Anchor:=Cells(i, 1), _
 	 Address:="", _
-	 'Add the hyperlinks for each individual worksheet name. To include chart sheets, use the 'Sheets' collection
 	 SubAddress:="'" & Worksheets(i).Name & "'!A1", _
 	 TextToDisplay:=Worksheets(i).Name
  Next i
